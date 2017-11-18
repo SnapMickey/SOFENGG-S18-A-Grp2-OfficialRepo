cfaef40989adccc0319128ecbc56550c4fa9414b
Jrmlmnq <jerome_lominoque@dlsu.edu.ph>
2017-11-19 00:06:15 +0800

    Added more data

--- pc_reservation.sql
+++ pc_reservation.sql
@@ -63,7 +63,7 @@ CREATE TABLE `lab` (
 
 LOCK TABLES `lab` WRITE;
 /*!40000 ALTER TABLE `lab` DISABLE KEYS */;
-INSERT INTO `lab` VALUES (1,'Henry Sy - 6th Floor','Henry Sy',0,NULL),(2,'Henry Sy - 7th Floor','Henry Sy',0,NULL),(3,'Henry Sy - 8th Floor','Henry Sy',1,NULL),(4,'Gokongwei Lab - G301A','Gokongwei',0,NULL),(5,'Gokongwei Lab - G302B','Gokongwei',1,NULL),(6,'Gokongwei Lab - G301B','Gokongwei',0,NULL),(7,'Gokongwei Lab - G302A','Gokongwei',1,NULL),(8,'Henry Sy - 9th Floor','Henry Sy',1,NULL),(9,'Henry Sy - 10th Floor','Henry Sy',1,NULL);
+INSERT INTO `lab` VALUES (1,'Henry Sy - 6th Floor','Henry Sy',0,NULL),(2,'Henry Sy - 7th Floor','Henry Sy',0,NULL),(3,'Henry Sy - 8th Floor','Henry Sy',1,NULL),(4,'Gokongwei Lab - G301A','Gokongwei',0,NULL),(5,'Gokongwei Lab - G302B','Gokongwei',1,NULL),(6,'Gokongwei Lab - G301B','Gokongwei',0,NULL),(7,'Gokongwei Lab - G302A','Gokongwei',1,NULL),(8,'Henry Sy - 9th Floor','Henry Sy',1,NULL),(9,'Henry Sy - 10th Floor','Henry Sy',1,NULL),(10,'Velasco Lab - V103','Velasco',1,NULL),(11,'Velasco Lab - V208A','Velasco',1,NULL),(12,'Velasco Lab - V208B','Velasco',1,NULL),(13,'Velasco Lab - V301','Velasco',1,NULL);
 /*!40000 ALTER TABLE `lab` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,7 +88,7 @@ CREATE TABLE `pc_info` (
 
 LOCK TABLES `pc_info` WRITE;
 /*!40000 ALTER TABLE `pc_info` DISABLE KEYS */;
-INSERT INTO `pc_info` VALUES (1,1,0),(2,1,1),(3,1,1),(4,1,0),(5,1,1),(6,2,1),(7,2,0),(8,2,1),(9,2,0),(10,2,1),(11,2,1),(12,2,1),(13,2,1),(14,5,1),(15,5,1),(16,5,1),(17,5,1),(18,7,1),(19,7,1),(20,7,0),(21,7,1),(22,8,1),(23,8,1),(24,8,0),(25,8,0),(26,9,1),(27,9,1),(28,9,1),(29,9,1),(30,3,1),(31,3,1),(32,3,1),(33,3,1);
+INSERT INTO `pc_info` VALUES (1,1,0),(2,1,1),(3,1,1),(4,1,0),(5,1,1),(6,2,1),(7,2,0),(8,2,1),(9,2,0),(10,2,1),(11,2,1),(12,2,1),(13,2,1),(14,5,1),(15,5,1),(16,5,1),(17,5,1),(18,7,1),(19,7,1),(20,7,0),(21,7,1),(22,8,1),(23,8,1),(24,8,0),(25,8,0),(26,9,1),(27,9,1),(28,9,1),(29,9,1),(30,3,1),(31,3,1),(32,3,1),(33,3,1),(34,10,1),(35,10,1),(36,10,1),(37,10,1),(38,11,1),(39,11,1),(40,11,1),(41,11,1),(42,11,1),(43,12,1),(44,12,1),(45,12,1),(46,12,1),(47,13,1),(48,13,1),(49,13,1),(50,13,1),(51,13,1);
 /*!40000 ALTER TABLE `pc_info` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -119,7 +119,7 @@ CREATE TABLE `pc_reservations` (
 
 LOCK TABLES `pc_reservations` WRITE;
 /*!40000 ALTER TABLE `pc_reservations` DISABLE KEYS */;
-INSERT INTO `pc_reservations` VALUES (1,10912312,2,'2017-11-15 08:00:00','2017-11-15 09:00:00','9999-09-09 09:09:09','2017-11-15 06:30:00',0,'None'),(2,11634521,3,'2017-11-15 08:00:00','2017-11-15 09:00:00','9999-09-09 09:09:09','2017-11-15 06:30:00',0,'None'),(3,11534567,2,'2017-11-15 08:00:00','2017-11-15 09:00:00','9999-09-09 09:09:09','2017-11-15 07:30:00',0,'None'),(4,11265423,5,'2017-11-16 08:00:00','2017-11-16 09:00:00','9999-09-09 09:09:09','2017-11-15 07:00:00',0,'None'),(5,11412345,2,'2017-11-15 10:00:00','2017-11-15 11:00:00','9999-09-09 09:09:09','2017-11-15 07:00:00',0,'None'),(6,11324215,13,'2017-11-16 08:00:00','2017-11-16 09:00:00','9999-09-09 09:09:09','2017-11-16 07:00:00',0,'None'),(7,11425232,11,'2017-11-16 08:00:00','2017-11-16 09:00:00','9999-09-09 09:09:09','2017-11-16 06:00:00',0,'None'),(8,11012344,30,'2017-11-16 08:00:00','2017-11-16 09:00:00','9999-09-09 09:09:09','2017-11-16 06:00:00',0,'GitHub Seminar'),(9,11123451,30,'2017-11-16 09:00:00','2017-11-16 10:00:00','9999-09-09 09:09:09','2017-11-16 07:00:00',0,'SOFENGG Seminar'),(10,11251234,30,'2017-11-16 12:00:00','2017-11-16 13:00:00','9999-09-09 09:09:09','2017-11-16 10:00:00',0,'Marketing Seminar');
+INSERT INTO `pc_reservations` VALUES (1,10912312,2,'2017-11-15 08:00:00','2017-11-15 09:00:00','9999-09-09 09:09:09','2017-11-15 06:30:00',0,'None'),(2,11634521,3,'2017-11-15 08:00:00','2017-11-15 09:00:00','9999-09-09 09:09:09','2017-11-15 06:30:00',0,'None'),(3,11534567,2,'2017-11-15 08:00:00','2017-11-15 09:00:00','9999-09-09 09:09:09','2017-11-15 07:30:00',0,'None'),(4,11265423,5,'2017-11-16 08:00:00','2017-11-16 09:00:00','9999-09-09 09:09:09','2017-11-15 07:00:00',0,'None'),(5,11412345,2,'2017-11-15 10:00:00','2017-11-15 11:00:00','9999-09-09 09:09:09','2017-11-15 07:00:00',0,'None'),(6,11324215,13,'2017-11-16 08:00:00','2017-11-16 09:00:00','9999-09-09 09:09:09','2017-11-16 07:00:00',0,'None'),(7,11425232,11,'2017-11-16 08:00:00','2017-11-16 09:00:00','9999-09-09 09:09:09','2017-11-16 06:00:00',0,'None'),(8,11012344,30,'2017-11-16 08:00:00','2017-11-16 09:00:00','9999-09-09 09:09:09','2017-11-16 06:00:00',0,'GitHub Seminar'),(9,11123451,30,'2017-11-16 09:00:00','2017-11-16 10:00:00','9999-09-09 09:09:09','2017-11-16 07:00:00',0,'SOFENGG Seminar'),(10,11251234,30,'2017-11-16 12:00:00','2017-11-16 13:00:00','9999-09-09 09:09:09','2017-11-16 10:00:00',0,'Marketing Seminar'),(11,11710821,36,'2017-11-16 08:00:00','2017-11-16 09:00:00','9999-09-09 09:09:09','2017-11-16 07:00:00',0,'None'),(12,11719191,48,'2017-11-16 12:00:00','2017-11-16 13:00:00','9999-09-09 09:09:09','2017-11-16 06:00:00',0,'None'),(13,11726351,50,'2017-11-16 12:00:00','2017-11-16 13:00:00','9999-09-09 09:09:09','2017-11-16 07:00:00',0,'None');
 /*!40000 ALTER TABLE `pc_reservations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -199,7 +199,7 @@ CREATE TABLE `user_info` (
 
 LOCK TABLES `user_info` WRITE;
 /*!40000 ALTER TABLE `user_info` DISABLE KEYS */;
-INSERT INTO `user_info` VALUES (10823512,'Tess Poñei','Iloveicecream_9','admin',NULL),(10912312,'Heverly Bien','Bienhev11*','faculty',NULL),(10923123,'Heinrich Lein','Heinlein_109','admin',NULL),(11012344,'Anna Bell','Bellanna_1','faculty',NULL),(11023124,'Larry Port','Larryport110*','admin',NULL),(11123451,'Nadroj Ajed','Nadrojedi12!','faculty',NULL),(11145342,'Perry Mon','dlsuemailpass123.','admin',NULL),(11251234,'Gary Go','Gary_dlsu123','faculty',NULL),(11265423,'Ellie Gomez','Gomellie_12','student',NULL),(11322213,'Han Cort','Hancortdlsu11!','faculty',NULL),(11324215,'Adam Tom','adam1_12_123!','student',NULL),(11412345,'John Ferry','123456abc!','student',NULL),(11425232,'Hagrid Henry','H@grid12','student',NULL),(11534567,'Mark Martinez','Hello123!','student',NULL),(11542311,'Lambert Alai','Lambertdlsu152!','faculty',NULL),(11634521,'Monique Lauff','Monlauff116*','student',NULL);
+INSERT INTO `user_info` VALUES (10823512,'Tess Poñei','Iloveicecream_9','admin',NULL),(10912312,'Heverly Bien','Bienhev11*','faculty',NULL),(10923123,'Heinrich Lein','Heinlein_109','admin',NULL),(11012344,'Anna Bell','Bellanna_1','faculty',NULL),(11023124,'Larry Port','Larryport110*','admin',NULL),(11123451,'Nadroj Ajed','Nadrojedi12!','faculty',NULL),(11145342,'Perry Mon','dlsuemailpass123.','admin',NULL),(11251234,'Gary Go','Gary_dlsu123','faculty',NULL),(11265423,'Ellie Gomez','Gomellie_12','student',NULL),(11322213,'Han Cort','Hancortdlsu11!','faculty',NULL),(11324215,'Adam Tom','adam1_12_123!','student',NULL),(11412345,'John Ferry','123456abc!','student',NULL),(11425232,'Hagrid Henry','H@grid12','student',NULL),(11490901,'Gaynelle Grady','Gradnelle007!','student',NULL),(11509152,'Cassie Troy','TroyCass09!','student',NULL),(11534567,'Mark Martinez','Hello123!','student',NULL),(11542311,'Lambert Alai','Lambertdlsu152!','faculty',NULL),(11611223,'Mitzi Frasier','FraMi116!','student',NULL),(11624523,'Jacklyn Pulido','Pulijack00%','student',NULL),(11634521,'Monique Lauff','Monlauff116*','student',NULL),(11710821,'Mallory Spivey','Spiderfan@11','student',NULL),(11712345,'Savanna Sisson','Sisson_123','student',NULL),(11719191,'Vena Burchfeld','Vburch117!','student',NULL),(11726351,'Claudette Morrow','Claudow23!','student',NULL);
 /*!40000 ALTER TABLE `user_info` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -212,4 +212,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-11-18 22:29:58
+-- Dump completed on 2017-11-19  0:05:34
